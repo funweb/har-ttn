c18ab24f49786069a55157a9896802ea6a998036
antOS <943164605@qq.com>
2022-02-18 10:44:36 +0800

    update

--- test_network_synthetic.py
+++ test_network_synthetic.py
@@ -8,82 +8,82 @@ from configuration import configure, general
 from configuration.general import placeholder_inputs, load_data
 
 
-def test_ttn(dict_cus):
+def test_ttn(dict_cus, k, weights_name=""):
     datadir = os.path.join(configure.parameters_dict["database_dir"], 'ende',
                            dict_cus["dataset_name"],
                            str(dict_cus["distance_int"]),
                            'npy')
     cutdatadir = os.path.join(datadir, str(configure.parameters_dict["ksplit"]))
 
-    for k in range(configure.parameters_dict["ksplit"]):
 
-        # TODO: load data
-        x_train, y_train, x_test, y_test, y_true, nb_classes, y_true_train, enc = load_data(
-            dict_cus["dataset_name"],
-            cutdatadir,
-            dict_cus["seq_len"],
-            k)
+    x_train, y_train, x_test, y_test, y_true, nb_classes, y_true_train, enc = load_data(
+        dict_cus["dataset_name"],
+        cutdatadir,
+        dict_cus["seq_len"],
+        k)
 
-        num_classes = nb_classes
+    num_classes = nb_classes
 
-        configure.parameters_dict["num_train"] = 0
-        configure.parameters_dict["num_classes"] = num_classes
-        general.Merge(configure.parameters_dict, dict_cus)
+    configure.parameters_dict["num_train"] = 0
+    configure.parameters_dict["num_classes"] = num_classes
+    general.Merge(configure.parameters_dict, dict_cus)
 
-        batch_size = 1
-        seq_len = dict_cus["seq_len"]
+    batch_size = 1
+    seq_len = dict_cus["seq_len"]
 
 
-        num_classes = nb_classes
-        # test_data = x_train
-        # test_label = y_train
-        test_data = x_test
-        test_label = y_test
+    num_classes = nb_classes
+    # test_data = x_train
+    # test_label = y_train
+    test_data = x_test
+    test_label = y_test
 
-        # test_data = scipy.io.loadmat('synthetic_data_test_2_gaussians.mat')['test_data']
-        # test_label = scipy.io.loadmat('synthetic_data_test_2_gaussians.mat')['test_label']
+    # test_data = scipy.io.loadmat('synthetic_data_test_2_gaussians.mat')['test_data']
+    # test_label = scipy.io.loadmat('synthetic_data_test_2_gaussians.mat')['test_label']
 
-        weights_dir = general.getWeightsDir(configure.parameters_dict, k)
-        checkpointPath = os.path.join(weights_dir, "%s_best_model" % (str(k)))
+    weights_dir = general.getWeightsDir(configure.parameters_dict, k)
 
-        assert os.path.exists(checkpointPath + ".index"), "请确认 iter/BS/len 等参数正确: {}".format(checkpointPath)
+    checkpointPath = os.path.join(weights_dir, "%s_best_model" % (str(k)) if weights_name == "" else weights_name)  # 是测试模型还是验证最后模型
 
-        with tf.Graph().as_default():
+    assert os.path.exists(checkpointPath + ".index"), "请确认 iter/BS/len 等参数正确: {}".format(checkpointPath)
 
-            # checkpointPath = 'weights/0_2160000_gaussians_github_ttn'  # TODO: 更改模型名称
+    with tf.Graph().as_default():
 
-            x_placeholder, y_placeholder, _ = placeholder_inputs(batch_size,  configure.parameters_dict["num_classes"], configure.parameters_dict["seq_len"])  # 类别数应该改为载入数据的格式
-            output, sequence_unwarped, gamma, sequence1 = network.mapping(x_placeholder, batch_size, configure.parameters_dict["seq_len"])
-            correct_prediction = tf.equal(tf.argmax(output, 1), tf.argmax(y_placeholder, 1))
+        # checkpointPath = 'weights/0_2160000_gaussians_github_ttn'  # 模型名称
 
-            saver = tf.train.Saver()
-            config = tf.ConfigProto()
-            config.gpu_options.allow_growth = True
-            sess = tf.Session(config=config)
-            saver.restore(sess, checkpointPath)
+        x_placeholder, y_placeholder, _ = placeholder_inputs(batch_size,  configure.parameters_dict["num_classes"], configure.parameters_dict["seq_len"])  # 类别数应该改为载入数据的格式
+        output, sequence_unwarped, gamma, sequence1 = network.mapping(x_placeholder, batch_size, configure.parameters_dict["seq_len"])
+        correct_prediction = tf.equal(tf.argmax(output, 1), tf.argmax(y_placeholder, 1))
 
-            correct = 0
-            generated_gamma = np.zeros((test_data.shape[0], seq_len))
-            ttn_output = np.zeros((test_data.shape[0], seq_len))
-            sequence_normalized = np.zeros((test_data.shape[0], seq_len))
+        saver = tf.train.Saver()
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth = True
+        sess = tf.Session(config=config)
+        saver.restore(sess, checkpointPath)
 
-            for testExample in range(test_data.shape[0]):
-                output_val, ttn_output_val, gamma_val, sequence1_val, correct_val = sess.run(
-                    [output, sequence_unwarped, gamma, sequence1, correct_prediction],
-                    feed_dict={x_placeholder: np.reshape(test_data[testExample, :], [1, seq_len]),
-                               y_placeholder: np.reshape(test_label[testExample, :], [1, num_classes])})
-                correct = correct + int(correct_val[0] == True)
+        correct = 0
+        generated_gamma = np.zeros((test_data.shape[0], seq_len))
+        ttn_output = np.zeros((test_data.shape[0], seq_len))
+        sequence_normalized = np.zeros((test_data.shape[0], seq_len))
 
-                generated_gamma[testExample, :] = gamma_val
-                ttn_output[testExample, :] = ttn_output_val
-                sequence_normalized[testExample, :] = np.squeeze(sequence1_val)
+        for testExample in range(test_data.shape[0]):
+            output_val, ttn_output_val, gamma_val, sequence1_val, correct_val = sess.run(
+                [output, sequence_unwarped, gamma, sequence1, correct_prediction],
+                feed_dict={x_placeholder: np.reshape(test_data[testExample, :], [1, seq_len]),
+                           y_placeholder: np.reshape(test_label[testExample, :], [1, num_classes])})
+            correct = correct + int(correct_val[0] == True)
 
-        accuracy = correct / float(test_data.shape[0])
+            generated_gamma[testExample, :] = gamma_val
+            ttn_output[testExample, :] = ttn_output_val
+            sequence_normalized[testExample, :] = np.squeeze(sequence1_val)
 
-        # scipy.io.savemat('./ttn_output_github.mat', mdict={'ttn_output': ttn_output})
-        # scipy.io.savemat('./generated_gamma_github.mat', mdict={'generated_gamma': generated_gamma})
-        # scipy.io.savemat('./sequence_normalized_github.mat', mdict={'sequence_normalized': sequence_normalized})
+    accuracy = correct / float(test_data.shape[0])
 
+    # scipy.io.savemat('./ttn_output_github.mat', mdict={'ttn_output': ttn_output})
+    # scipy.io.savemat('./generated_gamma_github.mat', mdict={'generated_gamma': generated_gamma})
+    # scipy.io.savemat('./sequence_normalized_github.mat', mdict={'sequence_normalized': sequence_normalized})
+
+    if weights_name == "":
         file_name = os.path.join(weights_dir, "%s_ttn_output_github.txt" % (str(k)))
         np.savetxt(file_name, np.array(ttn_output, dtype=np.float), delimiter=', ')
         print(general.colorstr("log saved at: {}".format(file_name)))
@@ -96,12 +96,9 @@ def test_ttn(dict_cus):
         np.savetxt(file_name, np.array(sequence_normalized, dtype=np.float), delimiter=', ')
         print(general.colorstr("log saved at: {}".format(file_name)))
 
+    print('Accuracy: {}\n\n'.format(accuracy))
 
-
-        print('Accuracy:')
-        print(accuracy)
-
-        break
+    return accuracy
 
 
 if __name__ == '__main__':
@@ -113,4 +110,6 @@ if __name__ == '__main__':
         "dataset_name": "cairo",
     }
 
-    test_ttn(dict_cus)
+    k = 0
+
+    test_ttn(dict_cus, k, weights_name="")
--- train_network_synthetic.py
+++ train_network_synthetic.py
@@ -1,3 +1,4 @@
+import json
 import shutil
 
 import tensorflow as tf
@@ -10,6 +11,7 @@ from configuration import configure
 from configuration.general import placeholder_inputs, load_data
 
 from configuration import general
+from test_network_synthetic import test_ttn
 
 
 def train_ttn(dict_cus):
@@ -19,13 +21,15 @@ def train_ttn(dict_cus):
                            'npy')
     cutdatadir = os.path.join(datadir, str(configure.parameters_dict["ksplit"]))
 
+    val_acc_dict = {}
+
     for k in range(configure.parameters_dict["ksplit"]):
-        #TODO: load data
+        # load data
         x_train, y_train, x_test, y_test, y_true, nb_classes, y_true_train, enc = load_data(
-                dict_cus["dataset_name"],
-                cutdatadir,
-                dict_cus["seq_len"],
-                k)
+            dict_cus["dataset_name"],
+            cutdatadir,
+            dict_cus["seq_len"],
+            k)
 
         train_data = x_train
         train_label = y_train  # 需要为 onehot 编码格式
@@ -46,11 +50,13 @@ def train_ttn(dict_cus):
         num_classes = configure.parameters_dict["num_classes"]  # TODO: 类别数量, 根据需要设置
         seq_len = configure.parameters_dict["seq_len"]
 
-
         with tf.Graph().as_default():
-            x_placeholder, y_placeholder, learning_rate_placeholder = placeholder_inputs(batch_size, num_classes, configure.parameters_dict["seq_len"])
+            x_placeholder, y_placeholder, learning_rate_placeholder = placeholder_inputs(batch_size, num_classes,
+                                                                                         configure.parameters_dict[
+                                                                                             "seq_len"])
 
-            output, sequence_unwarped, gamma, sequence1 = network.mapping(x_placeholder, batch_size, configure.parameters_dict["seq_len"])
+            output, sequence_unwarped, gamma, sequence1 = network.mapping(x_placeholder, batch_size,
+                                                                          configure.parameters_dict["seq_len"])
             loss = network.loss(output, y_placeholder)
 
             var_list_ttn = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='ttn')
@@ -73,7 +79,7 @@ def train_ttn(dict_cus):
             c_loss = 9999  # 初始化 loss
             train_loss = []
 
-            for step in range(maxIters*numBatches+1):
+            for step in range(maxIters * numBatches + 1):
                 batchIdx = batchIdx % numBatches
 
                 if batchIdx == 0:
@@ -92,36 +98,74 @@ def train_ttn(dict_cus):
                 train_loss.append(loss_value)
 
                 if step % 1000 == 0:
-                #     print('---------------------------------')
-                #     print(step)
-                #     print(loss_value)
-                    print("k: {}\tstep: {}/{}, loss_value: {}".format(k, step, maxIters*numBatches, loss_value))
+                    #     print('---------------------------------')
+                    #     print(step)
+                    #     print(loss_value)
+                    print("k: {}\tstep: {}/{}, loss_value: {}".format(k, step, maxIters * numBatches, loss_value))
 
                 batchIdx = batchIdx + 1
-                if step in np.linspace(0, maxIters*numBatches, 6):
+                if step in np.linspace(0, maxIters * numBatches, 6):
                     # weights_dir = os.path.join("weights")
                     weights_dir = general.getWeightsDir(configure.parameters_dict, k)
                     general.create_folder(weights_dir, remake=True)
-                    weights_name = os.path.join(weights_dir, "%s_%s_gaussians_github_ttn_%s" % (str(k), str(step), str(loss_value)))
+                    weights_name = os.path.join(weights_dir,
+                                                "%s_%s_gaussians_github_ttn_%s" % (str(k), str(step), str(loss_value)))
                     saver.save(sess, weights_name)
                     print(general.colorstr("model saved at: {}".format(weights_name)))
+
+                    val_acc = test_ttn(dict_cus, k, weights_name="%s_%s_gaussians_github_ttn_%s" % (
+                    str(k), str(step), str(loss_value)))
+                    val_acc_dict.update({"%s" % (str(k)):
+                                            {
+                                                "%s" % (str(step)): val_acc
+                                            }
+                                        })
 
                     if loss_value < c_loss:
                         weights_name = os.path.join(weights_dir, "%s_best_model" % (str(k)))
                         saver.save(sess, weights_name)
                         print(general.colorstr("model saved at: {}".format(weights_name)))
 
+                        val_acc = test_ttn(dict_cus, k, weights_name="%s_best_model" % (str(k)))
+                        val_acc_dict.update({"%s" % (str(k)):
+                                                {
+                                                    "best": val_acc
+                                                }
+                                            })
+
             weights_name = os.path.join(weights_dir, "%s_last_model" % (str(k)))
             saver.save(sess, weights_name)
             print(general.colorstr("model saved at: {}".format(weights_name)))
+
+            val_acc = test_ttn(dict_cus, k, weights_name="%s_last_model" % (str(k)))
+            val_acc_dict.update({"%s" % (str(k)):
+                                    {
+                                        "last": val_acc
+                                    }
+                                })
+
+            print(val_acc_dict)
+            val_acc_json_name = os.path.join(weights_dir, "%s_val_acc.json" % (str(k)))
+            with open(val_acc_json_name, "w", encoding="utf-8") as fw:
+                json.dump(val_acc_dict, fw)
+            print(general.colorstr("val_acc_json saved at: {}".format(val_acc_json_name)))
+
             log_name = os.path.join(weights_dir, "%s_train_loss.txt" % (str(k)))
             np.savetxt(log_name, np.array(train_loss, dtype=np.float), delimiter=', ')
             print(general.colorstr("log saved at: {}".format(log_name)))
 
+
 if __name__ == '__main__':
+
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+    # 默认为0：输出所有log信息
+    # 设置为1：进一步屏蔽INFO信息
+    # 设置为2：进一步屏蔽WARNING信息
+    # 设置为3：进一步屏蔽ERROR信息
+
     dict_cus = {
         "batch_size": 32,
-        "maxIters": 20,  # 100000
+        "maxIters": 100000,  # 100000
         "seq_len": 1024,
         "distance_int": 9999,
         "dataset_name": "cairo",
