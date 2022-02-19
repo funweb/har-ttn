0a70eb36eed05ead1fa71672da6c0fe0cedeccd5
antOS <943164605@qq.com>
2022-02-19 18:20:02 +0800

    update

--- train_network_synthetic.py
+++ train_network_synthetic.py
@@ -144,7 +144,6 @@ def train_ttn(dict_cus):
                 }
             )
 
-            # print(val_acc_dict)
             val_acc_json_name = os.path.join(weights_dir, "%s_val_acc.json" % (str(k)))
             with open(val_acc_json_name, "w", encoding="utf-8") as fw:
                 json.dump(val_acc_dict, fw)
@@ -154,6 +153,7 @@ def train_ttn(dict_cus):
             np.savetxt(log_name, np.array(train_loss, dtype=np.float), delimiter=', ')
             print(general.colorstr("log saved at: {}".format(log_name)))
 
+    print(val_acc_dict)
     print("\n\nk:\tbest\tlast\n0:\t{}\t{}\n1:\t{}\t{}\n2:\t{}\t{}\n".format(
                 val_acc_dict["0"]["best"], val_acc_dict["0"]["last"],
                 val_acc_dict["1"]["best"], val_acc_dict["1"]["last"],
